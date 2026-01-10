4c5cc2d4d8a9a54901df87e8d7357b8a03b7ac70
sveta1818 <faizulzhanova@gmail.com>
2026-01-11 02:55:48 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install',()=>{
+console.log('SW installed');
+})
+self.addEventListener('activate',()=>{
+    console.log('SW activated');
+})
+self.addEventListener('fetch',()=>{
+    
+})
